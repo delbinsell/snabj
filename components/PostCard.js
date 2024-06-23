b99f008b63503a1f9c22018255cd82e06de647f9
Anderson <aaavila241@misena.edu.co>
2024-06-23 09:37:22 -0500

    button publics

--- components/PostCard.js
+++ components/PostCard.js
@@ -282,13 +282,17 @@ export default function PostCard({id,content,created_at,photos,profiles:authorPr
             <input
               value={commentText}
               onChange={ev => setCommentText(ev.target.value)}
-              className="block w-full p-3 px-4 overflow-hidden h-12 rounded-full" placeholder="Leave a comment"/>
+              className="block w-full p-3 px-4 overflow-hidden h-12 rounded-full"
+              placeholder="Dejar un comentario"
+            />
+            <button
+              type="submit"
+              className="absolute top-0 right-3 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full shadow-md"
+              style={{ marginTop: '4px' }} // Ajusta la distancia hacia arriba según sea necesario
+            >
+              Enviar
+            </button>
           </form>
-          <button className="absolute top-3 right-3 text-gray-400">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-          </button>
         </div>
       </div>
       <div>
